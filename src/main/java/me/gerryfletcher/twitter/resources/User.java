a64eea0cd3961c8b9d295529f12040046c57ed8f
gerryfletch <1997Gerry@gmail.com>
2017-06-09 22:54:58 +0100

    Added full JWT authentication. Login works as intended.

---
+++ src/main/java/me/gerryfletcher/twitter/resources/User.java
@@ -0,0 +1,7 @@
+package me.gerryfletcher.twitter.resources;
+
+/**
+ * Created by Gerry on 09/06/2017.
+ */
+public class user {
+}
---
+++ src/main/java/me/gerryfletcher/twitter/sqlite/SQLUtils.java
@@ -0,0 +1,7 @@
+package me.gerryfletcher.twitter.sqlite;
+
+/**
+ * Created by Gerry on 09/06/2017.
+ */
+public class SQLUtils {
+}

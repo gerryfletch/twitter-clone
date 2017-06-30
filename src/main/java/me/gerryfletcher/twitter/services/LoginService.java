8f5049a834078a20235385ddd13bce4a0c1833ff
gerryfletch <1997Gerry@gmail.com>
2017-06-30 20:49:50 +0100

    Updated the login resource to follow new architecture.

--- src/main/java/me/gerryfletcher/twitter/services/LoginService.java
+++ src/main/java/me/gerryfletcher/twitter/services/LoginService.java
@@ -1,39 +1,27 @@
 package me.gerryfletcher.twitter.services;
 
-import me.gerryfletcher.twitter.controllers.security.JWTSecret;
-import me.gerryfletcher.twitter.controllers.sqlite.SQLUtils;
+import me.gerryfletcher.twitter.DAO.LoginDao;
+import me.gerryfletcher.twitter.exceptions.ApplicationException;
 import me.gerryfletcher.twitter.exceptions.BadDataException;
 import me.gerryfletcher.twitter.exceptions.UserNotExistsException;
-import me.gerryfletcher.twitter.models.Handle;
-import me.gerryfletcher.twitter.models.Password;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class LoginService {
 
     private static LoginService instance = null;
+    private LoginDao loginDao;
 
-    private static Connection conn = SQLUtils.connect();
-
-    protected LoginService() throws SQLException {}
+    protected LoginService(){
+        this.loginDao = new LoginDao();
+    }
 
     public static LoginService getInstance() {
         if(instance == null) {
-            try {
-                instance = new LoginService();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+            instance = new LoginService();
         }
 
         return instance;
     }
-
-    private final String check_user_SQL = "SELECT id, password, role FROM users WHERE lower(handle)=?";
-    private final PreparedStatement check_user = conn.prepareStatement(check_user_SQL);
 
     /**
      * Checks if the handle and password are valid, then attempts login.
@@ -43,40 +31,13 @@ public class LoginService {
      * @throws UserNotExistsException If the user does not exist
      * @throws BadDataException If a detail is wrong
      */
-    public String loginUser(String handle, String password) throws UserNotExistsException, BadDataException, SQLException {
-
-        handle = handle.toLowerCase();
-
-        if(! Handle.isHandleValid(handle))
-            throw new BadDataException("Username is not valid.");
-
-        if(! Password.isPasswordValid(password))
-            throw new BadDataException("Password is not valid.");
-
-
+    public String loginUser(String handle, String password) throws BadDataException, ApplicationException, UserNotExistsException {
         try {
-            check_user.setString(1, handle);
-            ResultSet result = check_user.executeQuery();
-
-            if(! result.next()) {
-                throw new UserNotExistsException("User " + handle + " does not exist.");
-            }
-
-            int uid = result.getInt("id");
-            String role = result.getString("role");
-            String passwordResult = result.getString("password");
-
-            if(! Password.checkPassword(password, passwordResult)) {
-                throw new BadDataException("Incorrect password.");
-            }
-
-            return new JWTSecret().generateToken(uid, handle, role);
-
+            return loginDao.loginUser(handle, password);
         } catch (SQLException e) {
             e.printStackTrace();
-            throw new SQLException("Something went wrong logging in.");
+            throw new ApplicationException("Problem logging User in DAO.", e);
         }
-
     }
 
 }

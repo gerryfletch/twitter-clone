eec4a3c7f6c1d5997ff129255d21094f07dac7f7
gerryfletch <1997gerry@gmail.com>
2017-07-10 21:57:52 +0100

    Created feed resource. Changed some resource paths.

---
+++ src/main/java/me/gerryfletcher/twitter/resources/tweets/GetTweetsResource.java
@@ -0,0 +1,67 @@
+package me.gerryfletcher.twitter.resources.tweets;
+
+import com.google.gson.*;
+import me.gerryfletcher.twitter.controllers.security.HTTPRequestUtil;
+import me.gerryfletcher.twitter.controllers.security.JWTSecret;
+import me.gerryfletcher.twitter.exceptions.ApplicationException;
+import me.gerryfletcher.twitter.services.tweets.TweetService;
+import org.glassfish.grizzly.http.util.HttpUtils;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+@Path("feed")
+public class GetTweetsResource {
+
+    private Gson gson = new GsonBuilder()
+            .setPrettyPrinting()
+            .serializeNulls()
+            .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+            .create();
+
+    /**
+     * Gets a Tweet feed from a users point of view, with a specified number of tweets.
+     * If no params are supplied, it defaults to 10 posts at offset 0.
+     *
+     * Example: get/feed/?limit=100&page=1
+     *
+     * @param auth  The authorization header.
+     * @param page  The offset of results - page 1, page 2 etc.
+     * @param limit The number of tweets to return.
+     * @return  JSON representation of tweets.
+     */
+    @GET
+    @RolesAllowed("user")
+    public Response getUserFeed(@HeaderParam("authorization") String auth, @QueryParam("limit") Integer limit, @QueryParam("page") Integer page) {
+        String token = HTTPRequestUtil.getJWT(auth);
+        JWTSecret jwt = new JWTSecret();
+        int uid = jwt.getClaim(token, "uid").asInt();
+
+        if (page == null) {
+            page = 0;
+        }
+
+        page = page * 10;
+
+        if (limit == null) {
+            limit = 10;
+        }
+
+        TweetService tweetService = TweetService.getInstance();
+
+        try {
+            JsonArray tweets = tweetService.getUserFeed(uid, limit, page);
+            JsonObject res = new JsonObject();
+            res.add("tweets", tweets);
+            System.out.println(gson.toJson(res));
+
+            return Response.ok().entity(res).build();
+
+        } catch (ApplicationException e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+}
